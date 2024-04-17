db8fbf98132d08a1448b59ac1f3b49d418ae1830
darasheiba <darasheibaa@gmail.com>
2024-04-17 08:08:35 +0700

    aplikasi bank transfer dengan runtime configuration

--- modul8_1302223075/Program.cs
+++ modul8_1302223075/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Text.Json;
+using modul8_1302223075;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonFilePath = "/Users/darasheibamalikachoiriyyah/Projects/modul8_1302223075/modul8_1302223075/bank_transfer_config.json";
+        BankTransferConfig config;
+
+        using (FileStream fs = File.OpenRead(jsonFilePath))
+        {
+            config = JsonSerializer.Deserialize<BankTransferConfig>(fs);
+        }
+    }
+}
 
